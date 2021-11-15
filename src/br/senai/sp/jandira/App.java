ddc2559aaa4e651e9e1ecc519fb8e2edf86dd9ac
Leonardo Antunes <leonardoantunes1401@gmail.com>
2021-11-14 23:27:25 -0300

    criou a class GameRepository

--- src/br/senai/sp/jandira/App.java
+++ src/br/senai/sp/jandira/App.java
@@ -3,19 +3,22 @@ package br.senai.sp.jandira;
 import br.senai.sp.jandira.model.Console;
 import br.senai.sp.jandira.model.Developer;
 import br.senai.sp.jandira.model.Game;
+import br.senai.sp.jandira.repository.GameRepository;
 
 public class App {
 	
 	public static void main(String[] args) {
+		GameRepository games = new GameRepository();
 		
-		Game zelda = new Game("Zelda - Breath of the Wild", "An adventure game with a big open world and a lot of places"
+		Game zelda = new Game("Zelda - Breath of the Wild", "An adventure game with a big open world and alots of places"
 					+ " to explore", new Developer("Nintendo"), Console.SWITCH, false, 200);
 		
 		Game gta = new Game("GTA V", "An open world game where you can steal cars and kill people", 
 					new Developer("Rockstar"), Console.PS4, true, 80);
 		
-		System.out.println(zelda.toString());
-		System.out.println();
-		System.out.println(gta.toString());
+		games.addGame(zelda);
+		games.addGame(gta);
+		
+		games.getGameList().forEach((n) -> System.out.println(n.toString() + "\n"));
 	}
 }
---
+++ src/br/senai/sp/jandira/repository/GameRepository.java
@@ -0,0 +1,25 @@
+package br.senai.sp.jandira.repository;
+
+import java.util.ArrayList;
+
+import br.senai.sp.jandira.model.Game;
+
+public class GameRepository {
+	private ArrayList<Game> gameList;
+	
+	public GameRepository() {
+		gameList = new ArrayList<Game>();
+	}
+	
+	public ArrayList<Game> getGameList() {
+		return gameList;
+	}
+	
+	public Game getGameFromIndex(int index) {
+		return gameList.get(index);
+	}
+	
+	public void addGame(Game g) {
+		gameList.add(g);
+	}
+}
