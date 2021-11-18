8404bdc7b1348ba5182147c7522d9fb3514bce62
Leonardo Antunes <leonardoantunes1401@gmail.com>
2021-11-18 13:59:03 -0300

    Alterou class Developer e criou DeveloperRepository

--- src/br/senai/sp/jandira/App.java
+++ src/br/senai/sp/jandira/App.java
@@ -3,22 +3,26 @@ package br.senai.sp.jandira;
 import br.senai.sp.jandira.model.Console;
 import br.senai.sp.jandira.model.Developer;
 import br.senai.sp.jandira.model.Game;
+import br.senai.sp.jandira.repository.DeveloperRepository;
 import br.senai.sp.jandira.repository.GameRepository;
 
 public class App {
 	
 	public static void main(String[] args) {
 		GameRepository games = new GameRepository();
+		DeveloperRepository developers = new DeveloperRepository();
 		
-		Game zelda = new Game("Zelda - Breath of the Wild", "An adventure game with a big open world and alots of places"
-					+ " to explore", new Developer("Nintendo"), Console.SWITCH, false, 200);
+		Game zelda = new Game("Zelda - Breath of the Wild", "An adventure game with a big open world and lots of places"
+					+ " to explore", developers.getDeveloper("Nintendo"), Console.SWITCH, false, 200);
 		
 		Game gta = new Game("GTA V", "An open world game where you can steal cars and kill people", 
-					new Developer("Rockstar"), Console.PS4, true, 80);
+					developers.getDeveloper("Rockstar"), Console.PS4, true, 80);
 		
 		games.addGame(zelda);
 		games.addGame(gta);
 		
 		games.getGameList().forEach((n) -> System.out.println(n.toString() + "\n"));
+		
+		System.out.println(developers.getDeveloper("Rockstar").toString());
 	}
 }
